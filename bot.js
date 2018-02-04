06f2c525149d971a2cf3a8e567657db4c4d98f25
fuerbringer <severin@protonmail.ch>
2018-02-04 19:23:22 +0100

    Fix issue where bot would not respond in group chats.

--- bot.js
+++ bot.js
@@ -6,17 +6,23 @@ const Telegraf = require('telegraf')
 const tldr = require('./lib/tldr')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.hears(/\/tldr (.+)/, ctx => {
-  const [command, parameter] = ctx.message.text.toLowerCase().split(' ')
-  const [platform, page] = parameter.split('/')
-  tldr.fetchPage(page, platform, (error, contents) => {
-    if (contents) {
-      return ctx.replyWithMarkdown(contents)
-    } else {
-      return ctx.reply("Sorry that page couldn't be found")
-      console.log(error)
-    }
-  })
-})
+bot.telegram.getMe().then((botInfo) => {
+  bot.options.username = botInfo.username
 
-bot.startPolling()
+  const deliverPage = ctx => {
+    const [command, parameter] = ctx.message.text.toLowerCase().split(' ')
+    const [platform, page] = parameter.split('/')
+    tldr.fetchPage(page, platform, (error, contents) => {
+      if (contents) {
+        return ctx.replyWithMarkdown(contents)
+      } else {
+        return ctx.reply("Sorry that page couldn't be found")
+        console.log(error)
+      }
+    })
+  }
+
+  bot.hears(/\/tldr (.+)/i, ctx => deliverPage(ctx))
+  bot.hears(new RegExp(`/tldr@${bot.options.username} (.+)`), ctx => deliverPage(ctx))
+  bot.startPolling()
+})
